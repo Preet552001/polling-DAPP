a6428f99d099d64d4032a029da4b59baacd2a7ad
Preet552001 <102042528+Preet552001@users.noreply.github.com>
2022-08-13 06:56:14 -0700

    Update polling_DAPP.sol

--- polling_DAPP.sol
+++ polling_DAPP.sol
@@ -37,10 +37,10 @@ contract polls_DAPP{
 
     function addPollOptions(uint _id,string calldata _name) external{
         require(polls[_id].creator == msg.sender,"User is Not the person to creat the poll");
-       uint8 count = polls[_id].optionCount;
-       count++;
-       Options[_id][count]=optionName(count,_name,0,0);
-       polls[_id].optionCount=count;
+        uint8 count = polls[_id].optionCount;
+        count++;
+        Options[_id][count]=optionName(count,_name,0,0);
+        polls[_id].optionCount=count;
     }
 
     function voting(uint _id, uint _option) external{
@@ -48,7 +48,7 @@ contract polls_DAPP{
         user[_id][msg.sender]=true;
         Options[_id][_option].numberOfVote=Options[_id][_option].numberOfVote+1;
         polls[_id].totalNumberOfUserVote=polls[_id].totalNumberOfUserVote+1;
-        Options[_id][_option].percentage = Options[_id][_option].numberOfVote * 100 / polls[_id].totalNumberOfUserVote;
+        Options[_id][_option].percentage = Options[_id][_option].numberOfVote * 100 / polls[_id].totalNumberOfUserVote; //In Solidity we can not store decimal value 
     }
 
 }
